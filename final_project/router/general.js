21fce2bcdd6a2fd0adbc2e24754ec013136fbaee
MicahC-404 <cermelem@gmail.com>
2026-01-06 12:35:43 -0500

    Implement user registration and book retrieval features

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,9 +4,19 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-public_users.post("/register", (req,res) => {
-  // Task 6 implementation will go here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 6: Register a new user
+public_users.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user. Provide username and password." });
 });
 
 // Task 1: Get the book list available in the shop
@@ -16,33 +26,50 @@ public_users.get('/', function (req, res) {
 
 // Task 2: Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  // Task 2 implementation will go here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    res.send(JSON.stringify(books[isbn], null, 4));
+  } else {
+    res.status(404).json({ message: "ISBN not found" });
+  }
 });
-  
+
 // Task 3: Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
   const keys = Object.keys(books);
-  let booksByAuthor = [];
+  let results = [];
 
   keys.forEach((key) => {
     if (books[key].author === author) {
-      booksByAuthor.push(books[key]);
+      results.push(books[key]);
     }
   });
 
-  if (booksByAuthor.length > 0) {
-    res.send(JSON.stringify(booksByAuthor, null, 4));
+  if (results.length > 0) {
+    res.send(JSON.stringify(results, null, 4));
   } else {
-    res.status(404).json({message: "No books found by this author"});
+    res.status(404).json({ message: "No books found by this author" });
   }
 });
 
 // Task 4: Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-  // Task 4 implementation will go here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const keys = Object.keys(books);
+  let results = [];
+
+  keys.forEach((key) => {
+    if (books[key].title === title) {
+      results.push(books[key]);
+    }
+  });
+
+  if (results.length > 0) {
+    res.send(JSON.stringify(results, null, 4));
+  } else {
+    res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
 // Task 5: Get book review
@@ -51,7 +78,7 @@ public_users.get('/review/:isbn', function (req, res) {
   if (books[isbn]) {
     res.send(JSON.stringify(books[isbn].reviews, null, 4));
   } else {
-    res.status(404).json({message: "No reviews found for this ISBN"});
+    res.status(404).json({ message: "No reviews found for this ISBN" });
   }
 });
 
